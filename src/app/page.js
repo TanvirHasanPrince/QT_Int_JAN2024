0673270ee5e8254f89288405a80da1d04034a3a5
unknown <t.hasan.prince@gmail.com>
2024-01-11 13:07:23 +0600

    Showing Popular Items

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
-import Image from 'next/image'
+
 import Navbar from './Components/Navbar/Navbar'
 import Banner from './Components/Banner/Banner'
+import Popular from './Components/Popular/Popular'
 
 export default function Home() {
   return (
    <div className='max-w-screen-xl m-auto'>
    <Navbar></Navbar>
    <Banner></Banner>
+   <Popular></Popular>
    </div>
   )
 }
